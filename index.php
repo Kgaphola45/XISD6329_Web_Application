18c753c26e732415ffb61892564c6640a047bbe5
Emmanuel kgaphola <it45ma@protonmail.ch>
2024-08-24 23:59:46 +0200

    Updated the footer, added links to another pages

--- index.php
+++ index.php
@@ -343,7 +343,18 @@
         <h6>About Us</h6>
         <p class="text-justify">Our old age home provides compassionate care and support for seniors. Our dedicated staff ensures a comfortable living environment with medical attention, recreational activities, and personalized care plans. We aim to enhance the quality of life for our residents through professional and heartfelt service.</p>
       </div>
-
+      
+      <div class="col-xs-6 col-md-3">
+        <h6>Services</h6>
+        <ul class="footer-links">
+          <li><a href="#">Medical Care</a></li>
+          <li><a href="#">Daily Assistance</a></li>
+          <li><a href="#">Recreational Activities</a></li>
+          <li><a href="#">Nutrition and Meals</a></li>
+          <li><a href="#">Family Support</a></li>
+          <li><a href="#">Safety and Security</a></li>
+        </ul>
+      </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>

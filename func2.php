da94738fe88e0a1ce167fc9ff385149b4a041466
Emmanuel kgaphola <it45ma@protonmail.ch>
2024-08-30 04:37:59 +0200

    Add function to save new input fields to the database

--- func2.php
+++ func2.php
@@ -1,251 +1,71 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","myhmsdb");
+$con = mysqli_connect("localhost", "root", "", "myhmsdb");
 
-if(isset($_POST['patsub1'])){
-	$fname=$_POST['fname'];
-  $lname=$_POST['lname'];
-  $gender=$_POST['gender'];
-  $email=$_POST['email'];
-  $contact=$_POST['contact'];
-	$password=$_POST['password'];
-  $cpassword=$_POST['cpassword'];
+if (isset($_POST['patsub1'])) {
+    // Elder data
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $contact = $_POST['contact'];
+    $email = $_POST['email'];
+    $medical_conditions = $_POST['medical_conditions'];
+    $allergies = $_POST['allergies'];
+    $current_medications = $_POST['current_medications'];
+    $primary_doctor_name = $_POST['primary_doctor_name'];
+    $doctor_contact = $_POST['doctor_contact'];
+    $special_needs = $_POST['special_needs'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
 
+    // Relative data
+    $relative_first_name = $_POST['relative_first_name'];
+    $relative_last_name = $_POST['relative_last_name'];
+    $relative_gender = $_POST['relative_gender'];
+    $relative_email = $_POST['relative_email'];
+    $relative_physical_address = $_POST['relative_physical_address'];
+    $relative_id_number = $_POST['relative_id_number'];
+    $relative_relation = $_POST['relative_relation'];
+    $relative_contact = $_POST['relative_contact'];
+    $relative_emergency_contact = $_POST['relative_emergency_contact'];
+    $relative_additional_notes = $_POST['relative_additional_notes'];
 
+    if ($password == $cpassword) {
+        // Insert elder data
+        $query = "INSERT INTO patreg (fname, lname, dob, gender, address, contact, email, medical_conditions, allergies, current_medications, primary_doctor_name, doctor_contact, special_needs, password, cpassword) 
+                  VALUES ('$fname', '$lname', '$dob', '$gender', '$address', '$contact', '$email', '$medical_conditions', '$allergies', '$current_medications', '$primary_doctor_name', '$doctor_contact', '$special_needs', '$password', '$cpassword')";
+        $result = mysqli_query($con, $query);
 
-  // Get data from form for registrations
-$first_name = $_POST['fname2'];
-$last_name = $_POST['lname2'];
-$id_number = $_POST['IDNumber'];
-$relation = $_POST['relation'];
-$gender = $_POST['gender'];
-$contact = $_POST['contact'];
-$emergency_contact = $_POST['emerPhone'];
+        if ($result) {
+            // Get the last inserted ID (pid)
+            $pid = mysqli_insert_id($con);
 
-  
-  if($password==$cpassword){
-  	$query="insert into patreg(fname,lname,gender,email,contact,password,cpassword) values ('$fname','$lname','$gender','$email','$contact','$password','$cpassword');";
-    $result=mysqli_query($con,$query);
-    if($result){
-        $_SESSION['username'] = $_POST['fname']." ".$_POST['lname'];
-        $_SESSION['fname'] = $_POST['fname'];
-        $_SESSION['lname'] = $_POST['lname'];
-        $_SESSION['gender'] = $_POST['gender'];
-        $_SESSION['contact'] = $_POST['contact'];
-        $_SESSION['email'] = $_POST['email'];
-        header("Location:admin-panel.php");
-    } 
+            // Insert relative data
+            $query2 = "INSERT INTO Relative_ToElderly (first_name, last_name, gender, email, physical_address, id_number, relation, contact, emergency_contact, additional_notes) 
+                       VALUES ('$relative_first_name', '$relative_last_name', '$relative_gender', '$relative_email', '$relative_physical_address', '$relative_id_number', '$relative_relation', '$relative_contact', '$relative_emergency_contact', '$relative_additional_notes')";
+            $result2 = mysqli_query($con, $query2);
 
-    $query1 = "select * from patreg;";
-    $result1 = mysqli_query($con,$query1);
-    if($result1){
-      $_SESSION['pid'] = $row['pid'];
+            if ($result2) {
+                $_SESSION['pid'] = $pid; 
+                $_SESSION['username'] = $fname . " " . $lname;
+                $_SESSION['fname'] = $fname;
+                $_SESSION['lname'] = $lname;
+                $_SESSION['gender'] = $gender;
+                $_SESSION['contact'] = $contact;
+                $_SESSION['email'] = $email;
+                header("Location:admin-panel.php");
+                exit();  
+            } else {
+                echo "Error inserting relative data: " . mysqli_error($con);
+            }
+        } else {
+            echo "Error inserting elder data: " . mysqli_error($con);
+        }
+    } else {
+        echo "Passwords do not match";
     }
-
-  }
-  else{
-    header("Location:error1.php");
-  }
-}
-if(isset($_POST['update_data']))
-{
-	$contact=$_POST['contact'];
-	$status=$_POST['status'];
-	$query="update appointmenttb set payment='$status' where contact='$contact';";
-	$result=mysqli_query($con,$query);
-	if($result)
-		header("Location:updated.php");
-}
-
-// Get data from form for registrations
-$first_name = $_POST['fname'];
-$last_name = $_POST['lname'];
-$id_number = $_POST['IDNumber'];
-$relation = $_POST['relation'];
-$gender = $_POST['gender'];
-$contact = $_POST['contact'];
-$emergency_contact = $_POST['emerPhone'];
-
-
-
-
-
-// function display_docs()
-// {
-// 	global $con;
-// 	$query="select * from doctb";
-// 	$result=mysqli_query($con,$query);
-// 	while($row=mysqli_fetch_array($result))
-// 	{
-// 		$name=$row['name'];
-// 		# echo'<option value="" disabled selected>Select Caregiver</option>';
-// 		echo '<option value="'.$name.'">'.$name.'</option>';
-// 	}
-// }
-
-if(isset($_POST['doc_sub']))
-{
-	$name=$_POST['name'];
-	$query="insert into doctb(name)values('$name')";
-	$result=mysqli_query($con,$query);
-	if($result)
-		header("Location:adddoc.php");
-}
-function display_admin_panel(){
-	echo '<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-  <a class="navbar-brand" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>  Golden Years Old Age Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-     <ul class="navbar-nav mr-auto">
-       <li class="nav-item">
-        <a class="nav-link" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>
-      </li>
-       <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0" method="post" action="search.php">
-      <input class="form-control mr-sm-2" type="text" placeholder="enter contact number" aria-label="Search" name="contact">
-      <input type="submit" class="btn btn-outline-light my-2 my-sm-0 btn btn-outline-light" id="inputbtn" name="search_submit" value="Search">
-    </form>
-  </div>
-</nav>
-  </head>
-  <style type="text/css">
-    button:hover{cursor:pointer;}
-    #inputbtn:hover{cursor:pointer;}
-  </style>
-  <body style="padding-top:50px;">
- <div class="jumbotron" id="ab1"></div>
-   <div class="container-fluid" style="margin-top:50px;">
-    <div class="row">
-  <div class="col-md-4">
-    <div class="list-group" id="list-tab" role="tablist">
-      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Appointment</a>
-      <a class="list-group-item list-group-item-action" href="patientdetails.php" role="tab" aria-controls="home">Elders List</a>
-      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Payment Status</a>
-      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Prescription</a>
-      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Caregivers Section</a>
-       <a class="list-group-item list-group-item-action" id="list-attend-list" data-toggle="list" href="#list-attend" role="tab" aria-controls="settings">Attendance</a>
-    </div><br>
-  </div>
-
-  
-
-
-
-
-
-  <div class="col-md-8">
-    <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-        <div class="container-fluid">
-          <div class="card">
-            <div class="card-body">
-              <center><h4>Create an appointment</h4></center><br>
-              <form class="form-group" method="post" action="appointment.php">
-                <div class="row">
-                  <div class="col-md-4"><label>First Name:</label></div>
-                  <div class="col-md-8"><input type="text" class="form-control" name="fname"></div><br><br>
-                  <div class="col-md-4"><label>Last Name:</label></div>
-                  <div class="col-md-8"><input type="text" class="form-control"  name="lname"></div><br><br>
-                  <div class="col-md-4"><label>Email id:</label></div>
-                  <div class="col-md-8"><input type="text"  class="form-control" name="email"></div><br><br>
-                  <div class="col-md-4"><label>Contact Number:</label></div>
-                  <div class="col-md-8"><input type="text" class="form-control"  name="contact"></div><br><br>
-                  <div class="col-md-4"><label>Caregiver:</label></div>
-                  <div class="col-md-8">
-                   <select name="doctor" class="form-control" >
-
-                     <!-- <option value="" disabled selected>Select Caregiver</option>
-                     <option value="Dr. Punam Shaw">Dr. Punam Shaw</option>
-                      <option value="Dr. Ashok Goyal">Dr. Ashok Goyal</option> -->
-                      <?php display_docs();?>
-
-
-
-
-                    </select>
-                  </div><br><br>
-                  <div class="col-md-4"><label>Payment:</label></div>
-                  <div class="col-md-8">
-                    <select name="payment" class="form-control" >
-                      <option value="" disabled selected>Select Payment Status</option>
-                      <option value="Paid">Paid</option>
-                      <option value="Pay later">Pay later</option>
-                    </select>
-                  </div><br><br><br>
-                  <div class="col-md-4">
-                    <input type="submit" name="entry_submit" value="Create new entry" class="btn btn-primary" id="inputbtn">
-                  </div>
-                  <div class="col-md-8"></div>                  
-                </div>
-              </form>
-            </div>
-          </div>
-        </div><br>
-      </div>
-      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-        <div class="card">
-          <div class="card-body">
-            <form class="form-group" method="post" action="func.php">
-              <input type="text" name="contact" class="form-control" placeholder="enter contact"><br>
-              <select name="status" class="form-control">
-               <option value="" disabled selected>Select Payment Status to update</option>
-                <option value="paid">paid</option>
-                <option value="pay later">pay later</option>
-              </select><br><hr>
-              <input type="submit" value="update" name="update_data" class="btn btn-primary">
-            </form>
-          </div>
-        </div><br><br>
-      </div>
-      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
-        <form class="form-group" method="post" action="func.php">
-          <label>Caregivers name: </label>
-          <input type="text" name="name" placeholder="enter doctors name" class="form-control">
-          <br>
-          <input type="submit" name="doc_sub" value="Add Caregiver" class="btn btn-primary">
-        </form>
-      </div>
-       <div class="tab-pane fade" id="list-attend" role="tabpanel" aria-labelledby="list-attend-list">...</div>
-    </div>
-  </div>
-</div>
-   </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <!--Sweet alert js-->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.all.js"></script>
-   <script type="text/javascript">
-   $(document).ready(function(){
-   	swal({
-  title: "Welcome!",
-  text: "Have a nice day!",
-  imageUrl: "images/sweet.jpg",
-  imageWidth: 400,
-  imageHeight: 200,
-  imageAlt: "Custom image",
-  animation: false
-})</script>
-  </body>
-</html>';
 }
 ?>
+

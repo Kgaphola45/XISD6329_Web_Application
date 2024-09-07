d75559ca9cbe93b220863116ab74d390cd7b3682
Emmanuel kgaphola <it45ma@protonmail.ch>
2024-09-07 15:51:53 +0200

    "Updated login.php: added title, favicon, and stylesheet links; modified head section; added JavaScript functions; and updated navbar with new links and styles."

--- login.php
+++ login.php
@@ -3,23 +3,116 @@ include("header.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
+<title>Golden Years Old Age Home</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-    <link rel="stylesheet" type="text/css" href="style2.css">
+    <style>
+        .form-control {
+            border-radius: 0.75rem;
+        }
+        .welcome-section {
+            text-align: center;
+            margin-top: 50px;
+            color: white;
+        }
+        .services-section, .contact-section {
+            margin-top: 50px;
+            margin-left: auto; 
+            margin-right: auto; 
+            max-width: 1200px; 
+            text-align: center;
+            color: white;
+        }
+        .services-section img, .welcome-section img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 10px;
+        }
+ 
+        h3 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+     
+    </style>
 
 
-    
-  </head>
-  
+
+    <script>
+        var check = function() {
+            if (document.getElementById('password').value ==
+                document.getElementById('cpassword').value) {
+                document.getElementById('message').style.color = '#5dd05d';
+                document.getElementById('message').innerHTML = 'Matched';
+            } else {
+                document.getElementById('message').style.color = '#aa4f4f';
+                document.getElementById('message').innerHTML = 'Not Matching';
+            }
+        }
+
+        function alphaOnly(event) {
+            var key = event.keyCode;
+            return ((key >= 65 && key <= 90) || key == 8 || key == 32);
+        };
+
+        function checklen() {
+            var pass1 = document.getElementById("password");
+            if (pass1.value.length < 6) {
+                alert("Password must be at least 6 characters long. Try again!");
+                return false;
+            }
+            return true; 
+        }
+    </script>
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" style="background-color: #007bff;">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#" style="margin-top: 10px; margin-left:-65px; font-family: 'IBM Plex Sans', sans-serif;">
+            <h4><i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp;Golden Years Old Age Home</h4>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item" style="margin-right: 20px;">
+                    <a class="nav-link js-scroll-trigger" href="index.php" style="color: white; font-family: 'IBM Plex Sans', sans-serif;">
+                        <h6>HOME</h6>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right: 20px;">
+                    <a class="nav-link js-scroll-trigger" href="index1.php" style="color: white; font-family: 'IBM Plex Sans', sans-serif;">
+                        <h6>SIGN UP</h6>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right: 20px;">
+                    <a class="nav-link js-scroll-trigger" href="services.html" style="color: white; font-family: 'IBM Plex Sans', sans-serif;">
+                        <h6>ABOUT US</h6>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right: 20px;">
+                    <a class="nav-link js-scroll-trigger" href="contact.html" style="color: white; font-family: 'IBM Plex Sans', sans-serif;">
+                        <h6>CONTACT</h6>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="gallery.php" style="color: white; font-family: 'IBM Plex Sans', sans-serif;">
+                        <h6>GALLERY</h6>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 
     <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">

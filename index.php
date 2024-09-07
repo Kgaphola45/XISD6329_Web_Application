04f9166d047bdf725aeb2c070ead675163105268
Emmanuel kgaphola <it45ma@protonmail.ch>
2024-09-07 08:54:53 +0200

    Updated footer CSS and HTML: changed footer background color, padding, and font styles; replaced social media icons with images; and reformatted social media links.

--- index.php
+++ index.php
@@ -327,14 +327,14 @@
          <a href="#">Our Old Age Home</a>.
         </p>
       </div>
-
+   
       <div class="col-md-4 col-sm-6 col-xs-12">
-        <ul class="social-icons">
-          <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-        </ul>
-      </div>
+  <ul class="social-icons">
+    <li><a class="facebook" href="#"><img src="images/footer Icons/facebook.png" alt="Facebook"></a></li>
+    <li><a class="twitter" href="#"><img src="images/footer Icons/twitter.png" alt="Twitter"></a></li>
+    <li><a class="linkedin" href="#"><img src="images/footer Icons/linkedIn.png" alt="LinkedIn"></a></li>
+  </ul>
+</div>
     </div>
   </div>
 </footer>
